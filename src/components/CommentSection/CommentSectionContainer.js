a5862a7efb19dc79600067358e98fc511735bc04
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-09 13:36:12 -0500

    rendered components on page

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,16 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const {comments} = props
+  
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {
+        comments.map(comment =>{
+         return <Comment key={comment.text} comment={comment}/>
+        })
+      }
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,22 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-
+import postDb from '../../dummy-data'
 const PostsPage = () => {
   // set up state for your data
+  const [posts, setPosts] = useState(postDb);
+  
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {
+        posts.map(post =>{
+          return <Post key={post.timestamp} post={post}/>
+        })
+      }
     </div>
   );
 };
