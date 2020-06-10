d9fa9884afb85b6c955a1b3924ea6a50d20f352d
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-10 15:47:08 -0500

    coment feature working

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -1,23 +1,31 @@
 // You do not need to do anything in this file
-import React from 'react';
+import React, {useState} from 'react';
 import CommentSection from './CommentSectionContainer';
 
 const CommentInput = props => {
 
   const { newComment, comments } = props
 
-  // const changeHandler = event => {
-  //   newComment([...comments, event.target.value])
-  // }
+  const [commentValue, setCommentValue] = useState('')
 
+  const changeHandler = event => {
+    // debugger
+    setCommentValue(event.target.value)
+  }
+
+  const submitHandler = event => {
+    // debugger
+    event.preventDefault()
+    newComment([...comments, {text: commentValue, username:'Me'}])
+  }
 
   return (
-    <form className="comment-form" onSubmit={props.submitComment}>
+    <form className="comment-form" onSubmit={submitHandler}>
       <input
         type="text"
         value={props.comment}
         placeholder="Add comment... "
-        // onChange={changeHandler}
+        onChange={changeHandler}
       />
     </form>
   );
